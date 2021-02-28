5c375162c397297beedb4e28427838a6741241d5
Louise Kirkham <louise.kirkham@royalmail.com>
2021-02-28 16:09:31 +0000

    Replaced content.py script wiith run_script and updated paths.

--- run_script.py
+++ run_script.py
@@ -3,14 +3,14 @@ import git
 from git_contributions_importer import *
 
 # Your private repo or Bitbucket repo
-repo = git.Repo("/Users/louisekirkham/Documents/Projects/data-science-models---docker")
+repo = git.Repo('/Users/louisekirkham/Documents/Projects/data-science-models---docker')
 
 # Your mock repo
-mock_repo = git.Repo("/Users/louisekirkham/Documents/Projects/github_mock_repos/mock_repo_rm_ds_models_docker")
+mock_repo = git.Repo('/Users/louisekirkham/Documents/Projects/github_mock_repos/mock_repo_rm_ds_models_docker')
 importer = Importer([repo], mock_repo)
 
 # I use both my personal email and work email here,
 # Since the private repo uses work email, and Github profiles uses
 # my work email
 importer.set_author(['louisek@gmail.com', 'louise.kirkham@royalmail.com'])
-importer.import_repository()
+
